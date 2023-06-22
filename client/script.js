63dbf6d14ecd71aaf596f2a978d8ad3dbbd0d074
Bhavik Patel <bhavikpatel@Bhaviks-MacBook-Air.local>
2023-06-22 18:29:43 -0400

    second commit

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:3000', {
+    const response = await fetch('https://gpt-backend-aeso.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
